03b61118ed3fee644fe0e0d041ce934c745e2355
Garrett Cox <garrettjcox@gmail.com>
2016-12-13 14:01:54 -0600

    Now Playing

--- src/views/home/homeC.js
+++ src/views/home/homeC.js
@@ -3,7 +3,13 @@
     .module('skeleton')
     .controller('homeC', homeC);
 
-  function homeC() {
-
+  function homeC(nowPlaying) {
+    nowPlaying.get('garrettjoecox')
+      .then(res => {
+        this.nowPlaying = {
+          song: res.name,
+          artist: res.artist['#text']
+        };
+      });
   }
 })();
